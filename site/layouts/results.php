ce28a132838c7fbdd348833d1107459289bd9cac
JoomShaper <support@joomshaper.com>
2016-01-05 16:54:47 +0600

    Added copyright information

--- site/layouts/results.php
+++ site/layouts/results.php
@@ -1,6 +1,13 @@
 <?php
-
-defined('JPATH_BASE') or die;
+/*------------------------------------------------------------------------
+# SP Polls - Ajax Poll Component by JoomShaper.com
+# ------------------------------------------------------------------------
+# author    JoomShaper http://www.joomshaper.com
+# Copyright (C) 2010 - 2016 JoomShaper.com. All Rights Reserved.
+# License - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://www.joomshaper.com
+-------------------------------------------------------------------------*/
+defined ('_JEXEC') or die('resticted aceess');
 
 $options 	= $displayData['options'];
 $total_votes = 0;
