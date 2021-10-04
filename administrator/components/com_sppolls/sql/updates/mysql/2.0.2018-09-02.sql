caab20541033f3d9a42b7fa71d034613f1ef0262
Md Siddiqur Rahman <siddikcoder@gmail.com>
2021-10-04 12:10:44 +0600

    fix sql error

--- administrator/components/com_sppolls/sql/updates/mysql/2.0.2018-09-02.sql
+++ administrator/components/com_sppolls/sql/updates/mysql/2.0.2018-09-02.sql
@@ -7,10 +7,10 @@
 -- update on version 2.0
 
 --change #__sppolls_polls table's columns
-ALTER TABLE `#__sppolls_polls` CHANGE COLUMN `sppolls_poll_id` `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
-ALTER TABLE `#__sppolls_polls` CHANGE COLUMN `slug` `alias` VARCHAR(255) NOT NULL DEFAULT '';
-ALTER TABLE `#__sppolls_polls` CHANGE COLUMN `enabled` `published` TINYINT(3) NOT NULL DEFAULT '1';
-ALTER TABLE `#__sppolls_polls` CHANGE COLUMN `created_on` `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
-ALTER TABLE `#__sppolls_polls` CHANGE COLUMN `modified_on` `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
-ALTER TABLE `#__sppolls_polls` CHANGE COLUMN `locked_by` `checked_out` bigint(20) NOT NULL DEFAULT '0';
-ALTER TABLE `#__sppolls_polls` CHANGE COLUMN `locked_on` `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
+ALTER TABLE `#__sppolls_polls` CHANGE `sppolls_poll_id` `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `#__sppolls_polls` CHANGE `slug` `alias` varchar(55) NOT NULL DEFAULT '';
+ALTER TABLE `#__sppolls_polls` CHANGE `enabled` `published` tinyint NOT NULL DEFAULT '1';
+ALTER TABLE `#__sppolls_polls` CHANGE `created_on` `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
+ALTER TABLE `#__sppolls_polls` CHANGE `modified_on` `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
+ALTER TABLE `#__sppolls_polls` CHANGE `locked_by` `checked_out` bigint NOT NULL DEFAULT '0';
+ALTER TABLE `#__sppolls_polls` CHANGE `locked_on` `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
---
+++ administrator/components/com_sppolls/sql/updates/mysql/3.0.0.2021-10-01.sql
@@ -0,0 +1,7 @@
+-- SP Polls - by JoomShaper.com
+-- author    JoomShaper http://www.joomshaper.com
+-- Copyright (C) 2010 - 2018 JoomShaper.com. All Rights Reserved.
+-- License - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+-- Websites: http://www.joomshaper.com
+
+-- update on version 3.0.0
