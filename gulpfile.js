471056ecf514b2b0bd3d429e2d294044e83f05fe
Md Siddiqur Rahman <53170094+siddik-web@users.noreply.github.com>
2025-11-17 15:36:52 +0600

    chore: update version to 3.2.0 in sppolls.xml and gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ function cleanBuild() {
 }
 
 function cleanZip() {
-	return src('./com_sppolls_v3.1.0.zip', { read: false, allowEmpty: true }).pipe(clean());
+	return src('./com_sppolls_v3.2.0.zip', { read: false, allowEmpty: true }).pipe(clean());
 }
 
 function copySite() {
@@ -74,7 +74,7 @@ function minify_admin_js() {
 }
 
 function makeZip() {
-	return src('./build/**/*.*').pipe(zip('com_sppolls_v3.1.0.zip')).pipe(dest('./'));
+	return src('./build/**/*.*').pipe(zip('com_sppolls_v3.2.0.zip')).pipe(dest('./'));
 }
 
 exports.copy = series(
